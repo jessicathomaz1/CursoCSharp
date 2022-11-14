6298815dd3602f1017f367229243d4b816e95229
Jéssica Thomaz <jessica.thomaz@vibetecnologia.com>
2022-11-14 12:57:19 -0300

    curso csharp

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class PrimeiroPrograma {
+        static void Main() {
+            Console.Write("Primeiro");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            {
+                Console.WriteLine("Hello World! 2");
+            }
+        }
+    }
+}
