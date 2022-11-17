f9ca30a299a3adbc58797afe8c6aab82c67133e9
Jéssica Thomaz <jessica.claudiat@gmail.com>
2022-11-17 16:22:33 -0300

    Estrutura While - Estruturas de Controle

---
+++ CursoCSharp/EstruturasDeControle/EstruturaWhile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle {
+    internal class EstruturaWhile {
+        public static void Executar()
+        {
+            int palpite = 0;
+            Random random = new Random();
+
+            int numeroSecreto = random.Next(1, 16);
+            bool numeroEncontrado = false;
+            int tentativasRestantes = 5;
+            int tentativas = 0;
+
+            while (tentativasRestantes > 0 && !numeroEncontrado) {
+                Console.WriteLine("Insira o seu palpite: ");
+                string entrada = Console.ReadLine();
+                int.TryParse(entrada, out palpite);
+
+                tentativas++;
+                tentativasRestantes--;
+
+                if (numeroSecreto == palpite) {
+                    numeroEncontrado = true;
+                    var corAnterior = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Número encontrado em {0} tentativas", tentativas);
+                    Console.BackgroundColor = corAnterior;
+                } else if (palpite > numeroSecreto) {
+                    Console.WriteLine("Menor... Tente novamente!");
+                    Console.WriteLine("Tentativas restantes {0}", tentativasRestantes);
+                } else {
+                    Console.WriteLine("Maior... Tente novamente!");
+                    Console.WriteLine("Tentativas restantes: {0}", tentativasRestantes);
+                }
+            }
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -25,6 +25,7 @@ namespace CursoCSharp {
                 {"Estrutura If/Else - Estruturas de Controle", EstruturaIfElse.Executar},
                 {"Estrutura If/Else/If - Estruturas de Controle", EstruturaIfElseIf.Executar},
                 {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
             });
 
             central.SelecionarEExecutar();
