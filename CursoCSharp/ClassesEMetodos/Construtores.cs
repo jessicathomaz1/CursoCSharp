2f03db202cb1bd1873a74e1aa16fcb7a94b5f803
Jéssica Thomaz <jessica.claudiat@gmail.com>
2022-11-17 23:25:19 -0300

    Construtores - Classes e Métodos

---
+++ CursoCSharp/ClassesEMetodos/Construtores.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class Carro {
+        public string Modelo;
+        public string Fabricante;
+        public int Ano;
+
+        // Construtor
+        public Carro(string modelo, string fabricante, int ano) {
+            Modelo = modelo;
+            Fabricante = fabricante;
+            Ano = ano;
+        }
+
+        // Construtor padrão
+        public Carro() {
+
+        }
+    }
+    internal class Construtores {
+        public static void Executar() {
+            var carro1 = new Carro();
+            carro1.Fabricante = "BMW";
+            carro1.Modelo = "352i";
+            carro1.Ano = 2020;
+            Console.WriteLine($"{carro1.Fabricante} {carro1.Modelo} {carro1.Ano}");
+
+            var carro2 = new Carro("Ka", "Ford", 2018);
+            Console.WriteLine($"{carro2.Fabricante} {carro2.Modelo} {carro2.Ano}");
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -37,6 +37,7 @@ namespace CursoCSharp {
 
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
             });
 
             central.SelecionarEExecutar();
