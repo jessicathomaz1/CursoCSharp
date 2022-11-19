140570757ec82e1c7ac1f00ead0098a926b430db
Jéssica Thomaz <jessica.claudiat@gmail.com>
2022-11-19 11:29:12 -0300

    Array List - Coleções

---
+++ CursoCSharp/Colecoes/ColecoesArrayList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Colecoes {
+    internal class ColecoesArrayList {
+        public static void Executar() {
+            var arrayLit = new ArrayList {
+                "Palavra",
+                3,
+                true
+            };
+
+            arrayLit.Add(3.14);
+
+            foreach(var item in arrayLit) {
+                Console.WriteLine("{0} => {1}", item, item.GetType());
+            }
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -54,6 +54,7 @@ namespace CursoCSharp {
                 // Coleções
                 {"Array - Coleções", Colecoes.Array.Executar},
                 {"List - Coleções", ColecoesList.Executar},
+                {"Array List - Coleções", ColecoesArrayList.Executar},
             });
 
             central.SelecionarEExecutar();
