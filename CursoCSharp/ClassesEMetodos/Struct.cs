383de09d44c4e4816873cd4ef792a5416e97ee7b
Jéssica Thomaz <jessica.claudiat@gmail.com>
2022-11-19 09:21:49 -0300

    Struct - Classes e Métodos

---
+++ CursoCSharp/ClassesEMetodos/Struct.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+    interface Ponto {
+        void MoverNaDiagonal(int delta) { }
+    }
+
+    struct Coordenada : Ponto {
+        public int X;
+        public int Y;
+
+        public Coordenada(int x, int y) {
+            X = x;
+            Y = y;
+        }
+
+        public void MoverNaDiagonal(int delta) {
+            X += delta;
+            Y += delta;
+        }
+    }
+    internal class ExemploStruct {
+        
+        public static void Executar() {
+            Coordenada coordenadaInicial;
+            coordenadaInicial.X = 2;
+            coordenadaInicial.Y = 2;
+
+            Console.WriteLine("Coordenada inicial: ");
+            Console.WriteLine("X = {0}", coordenadaInicial.X);
+            Console.WriteLine("Y = {0}", coordenadaInicial.Y);
+
+            var coordenadaFinal = new Coordenada(x: 9, y: 1);
+            coordenadaFinal.MoverNaDiagonal(10);
+
+            Console.WriteLine("Coordenada final:");
+            Console.WriteLine("X = {0}", coordenadaFinal.X);
+            Console.WriteLine("Y = {0}", coordenadaFinal.Y);
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -48,6 +48,7 @@ namespace CursoCSharp {
                 {"Propriedades - Classes e Métodos", Propriedades.Executar},
                 {"Readonly - Classes e Métodos", Readonly.Executar},
                 {"Enum - Classes e Métodos", ExemploEnum.Executar},
+                {"Struct - Classes e Métodos", ExemploStruct.Executar},
             });
 
             central.SelecionarEExecutar();
